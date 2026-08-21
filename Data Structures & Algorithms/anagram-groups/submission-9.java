a6137082db87052ac92f0ec67cce59f5f69574b4
williamrusya <96901322+williamrusya@users.noreply.github.com>
2026-08-21 13:50:43 +0500

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(String str : strs){
+            char[] ch = str.toCharArray();
+            Arrays.sort(ch);
+
+            String signature = new String(ch);
+
+            map.putIfAbsent(signature, new ArrayList<>());
+
+            map.get(signature).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    /*
+    1. Iterate through every string.
+    2. Create signature that identify its anagram group.
+    3. Use signature as a HashMap key.
+    4. Add the original string to the corresponding group.
+    5. Return all groups.
+    */
+}
